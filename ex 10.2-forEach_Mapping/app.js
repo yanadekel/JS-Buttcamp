97ba40b99b5a13f56879d993f762ba0916e33c6b
yanadekel <75662763+yanadekel@users.noreply.github.com>
2021-02-18 09:22:20 +0200

    commit

--- ex 10.2-forEach_Mapping/app.js
+++ ex 10.2-forEach_Mapping/app.js
@@ -11,9 +11,26 @@ itArr = [2, 4, 3, 9, 5, 3, 3];
 
 // console.log (doubleValues(itArr));
 
+// let doubleValues=(arr)=>{
+//   let newArr=[];
+//   arr.forEach(element => {
+//     newArr.push(element*2);
+//   });
+//   return newArr;
+// }
+
+// console.log(doubleValues(itArr));
+
 // 2. Write a function called onlyEvenValues which accepts an
 // array and returns a new array with only the even values in
 // the array passed to the function.
+
+// let onlyEvenValues= (array)=> {
+//    return array.filter ((j)=>{
+//     return j%2===0;
+//   })
+// }
+// console.log(onlyEvenValues(itArr));
 
 // let onlyEvenValues = (arr) =>{
 //   return arr.map ((num) => {
@@ -36,13 +53,27 @@ itArr = [2, 4, 3, 9, 5, 3, 3];
 // };
 
 // console.log(onlyEvenValues(itArr));
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 3. Write a function called showFirstAndLast which accepts
 // an array as an argument and returns a new array with only
 // the first and last elements from the function’s argument
 // array. The returned array should only contain elements that
 // are strings.
+
 // let go = [3, "l", 9, "dekel", "haim", 9, "shai"];
+
+// let showFirstAndLast = (array) => {
+//   let newFl = [];
+//   let temp = array.filter((element) => {
+//     return typeof element === "string";
+//   });
+//   newFl.push(temp.shift());
+//   newFl.push(temp.pop());
+//   return newFl;
+// };
+
+// console.log(showFirstAndLast(go));
+
 // let showFirstAndLast = (array) => {
 //   let coolArr = [];
 //   let temp = [];
@@ -58,6 +89,7 @@ itArr = [2, 4, 3, 9, 5, 3, 3];
 
 // console.log(showFirstAndLast(go));
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // 4. Write a function called vowelCount which accepts a
 // string as an argument. The function should return an object
@@ -65,7 +97,6 @@ itArr = [2, 4, 3, 9, 5, 3, 3];
 // the string. The key should be the vowel and the value
 // should be the count. e.g. {a:3, o:2,u:4}.
 // Should not be case sensitive.
-
 
 // let vowelCount= (str)=> {
 //
@@ -81,4 +112,61 @@ itArr = [2, 4, 3, 9, 5, 3, 3];
 
 // vowelCount("ldskdakcd");
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 5. Write a function capitalize that takes a string as an
+// argument and will return the whole string capitalized.
 
+// let capitalize = (str)=>{
+
+//   return str.toUpperCase();
+
+// }
+
+let capitalize = (str) => {
+  return str
+    .split("")
+    .map((char) => {
+      if (97 <= char.charCodeAt(0) && char.charCodeAt(0) <= 122) {
+        return String.fromCharCode(char.charCodeAt(0) - 32);
+      } else {
+        return char;
+      }
+    })
+    .join("");
+};
+
+// console.log(capitalize("Yana"));
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// 6. Write a function called shiftLetters that takes a string as
+// an argument and return’s an encoded string with each
+// letter shifted down the alphabet by one.
+
+// let shiftLetters = (str) => {
+//   return str.split("").map((char) => {
+//     return String.fromCharCode(char.charCodeAt(0) - 1);
+//   });
+// };
+
+// console.log(shiftLetters("abcdefg"));
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// 7. Create a function called swapCase that takes a string as
+// an argument and returns a string that every other word is
+// capitalized. (you can use the fifth’s exercise's function to
+// keep it dry)
+
+let swapCase = (str) => {
+  return str
+    .split(" ")
+    .map((j, i) => {
+      if (i % 2 === 0) {
+        return capitalize(j);
+      } else {
+        return j;
+      }
+    })
+    .join(" ");
+};
+
+console.log(swapCase("yana and shai forever"));
--- ex 16.1-closures/app.js
+++ ex 16.1-closures/app.js
@@ -4,11 +4,35 @@
 var b = 1;
 function someFunction(number) {
  function otherFunction(input) {
- return b;
+ return b;// not assigned  taking the vlue from perent fun and making closer:
  }
- b = 5;
+ b = 5;// making assign to inner function;
  return otherFunction;
 }
-var firstResult = someFunction(9);
-var result = firstResult(2);
+var firstResult = someFunction(9);// always will be 5 becuse the cluser .
+var result = firstResult(2);// always will return 5 becuse of the closer with child fun!
+
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var a = 1;
+function b2() {
+ a = 10;
+ return;//even we adding a to the return it in the scoop and run of inner function an not the global scoop.
+ function a() { }
+}
+b2();// undifind function the return is "undifind"; even we add "a" to the return it still on the inner scoop=fun scoop;
+console.log(a);// it refers the global scoop so always be eqel to a on the global scoop=1;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let i;
+for ( i = 0; i < 3; i++) {
+ const log = () => {
+ console.log(i);
+ }
+ setTimeout(log, 100);
+}
+//will be 3 times 3=> i*3 becouse it's makin another round and then stops;
+//the (i) on global. if we add 'let' to 'i' the then we make it to local scoop;
+ ///////////////////////////////////////////////////////
+
+
